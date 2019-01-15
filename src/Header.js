3715a3545b9830be6f6d064bafc71882b7c1626a
Julie Zembik <kurokurosan@gmail.com>
2019-01-15 15:59:41 -0600

    created header and was able to connect it to the App.js

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render () {
+        return (
+            <div>
+            <header>
+                <h1>React: Famous People</h1> 
+            </header>
+            </div>   
+        );
+    }
+}
+
+export default Header;
